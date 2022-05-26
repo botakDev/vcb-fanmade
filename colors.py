f9acd654d97678c64520b20f31ea72adf7f6463b
Robert Laskiewicz <botak192.128@gmail.com>
2022-05-26 21:33:01 +0200

    added new colors

--- colors.py
+++ colors.py
@@ -1,10 +1,13 @@
 import pygame
 
 
+BLACK = pygame.color.Color(0, 0, 0)
 WHITE = pygame.color.Color(255, 255, 255)
 GRAY = pygame.color.Color(41, 41, 41)
 RED = pygame.color.Color(161, 31, 41)
+RED_DARK = pygame.color.Color(153, 0, 0)
 RED_INACTIVE = pygame.color.Color(54, 18, 21)
+GREEN_LIGHT = pygame.color.Color(20, 201, 44)
 GREEN = pygame.color.Color(93, 168, 89)
 GREEN_INACTIVE = pygame.color.Color(87, 112, 85)
 YELLOW = pygame.color.Color(222, 222, 146)
